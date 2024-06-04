0a2a1c747fed114edf3da097aad0304f97bc161e
Asil Asaad Mufti <126459608+Asilmufti111@users.noreply.github.com>
2024-06-04 16:45:40 +0300

    Add files via upload

---
+++ LAB5/src/org/yourorghere/LAB5.java
@@ -0,0 +1,110 @@
+package org.yourorghere;
+
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.GLUT;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+/**
+ * LAB5.java <BR>
+ * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel)
+ * <P>
+ *
+ * This version is equal to Brian Paul's version 1.2 1999/10/21
+ */
+public class LAB5 implements GLEventListener {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame("Simple JOGL Application");
+        GLCanvas canvas = new GLCanvas();
+
+        canvas.addGLEventListener(new LAB5());
+        frame.add(canvas);
+        frame.setSize(640, 480);
+        final Animator animator = new Animator(canvas);
+        frame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                // Run this on another thread than the AWT event queue to
+                // make sure the call to Animator.stop() completes before
+                // exiting
+                new Thread(new Runnable() {
+
+                    public void run() {
+                        animator.stop();
+                        System.exit(0);
+                    }
+                }).start();
+            }
+        });
+        // Center frame
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        animator.start();
+    }
+
+    public void init(GLAutoDrawable drawable) {
+        // Use debug pipeline
+        // drawable.setGL(new DebugGL(drawable.getGL()));
+
+        GL gl = drawable.getGL();
+        System.err.println("INIT GL IS: " + gl.getClass().getName());
+
+        // Enable VSync
+        gl.setSwapInterval(1);
+
+        // Setup the drawing area and shading mode
+        gl.glClearColor(204 / 255f, 255 / 255f, 153 / 255f, 0.0f);
+        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL gl = drawable.getGL();
+        GLU glu = new GLU();
+
+        if (height <= 0) { // avoid a divide by zero error!
+
+            height = 1;
+        }
+        final float h = (float) width / (float) height;
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(45.0f, h, 1.0, 20.0);
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
+    public void display(GLAutoDrawable drawable) {
+        GL gl = drawable.getGL();
+        GLUT glut = new GLUT();
+
+        // Clear the drawing area
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        // Reset the current matrix to the "identity"
+        gl.glLoadIdentity();
+
+        // Move the "drawing cursor" around
+        gl.glTranslatef(0.0f, -1f, -6.0f);
+        gl.glColor3f(204 / 255f, 102 / 255f, 0 / 255f);// Set the current drawing color to brown
+        glut.glutSolidCube(1);//drow the cube
+        
+        // Move the "drawing cursor" to another position
+        gl.glTranslatef(0.0f, 1.2f, 0.0f);
+        gl.glColor3f(0f, 128 / 255f, 0f); // Set the current drawing color to green
+        glut.glutSolidSphere(1, 15, 15);
+
+        // Flush all drawing operations to the graphics card
+        gl.glFlush();
+    }
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }
+}
